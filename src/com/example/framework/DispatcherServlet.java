8667beafdd6c24795dbd3edc036bc6fee4c49ff8
AlphaTech23 <myahaintso@gmail.com>
2025-10-20 09:28:50 +0300

    sprint2

---
+++ src/com/example/framework/DispatcherServlet.java
@@ -0,0 +1,28 @@
+package com.example.framework;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class DispatcherServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        try {
+            dispatch(req, resp);
+        } catch(Exception e) {
+            throw new ServletException(e.getMessage());
+        }
+    }
+
+    private void dispatch(HttpServletRequest req, HttpServletResponse resp) throws Exception{
+        String url = req.getRequestURI().substring(req.getContextPath().length());
+        if(getServletContext().getResource(url) != null) {
+            getServletContext().getNamedDispatcher("default").forward(req, resp);
+        } else {
+            resp.getWriter().print("URL : " + url);
+        }
+    }
+}
