569ac6734ee71cbb92af36bf9374c9315b86861c
AlphaTech23 <myahaintso@gmail.com>
2025-11-17 09:28:34 +0300

    sprint4Bis

---
+++ src/com/example/framework/core/ModelView.java
@@ -0,0 +1,12 @@
+package com.example.framework.core;
+
+import java.util.HasMap;
+
+public class ModelView {
+    private String view;
+
+    public ModelView(String view) {
+        this.view = view;
+    }
+
+}
