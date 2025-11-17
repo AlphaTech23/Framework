5ba43b9c9ee23127778e57a0f2a284201486f564
AlphaTech23 <myahaintso@gmail.com>
2025-11-17 09:31:11 +0300

    sprint4Bis

--- src/com/example/framework/core/ModelView.java
+++ src/com/example/framework/core/ModelView.java
@@ -5,8 +5,20 @@ import java.util.HasMap;
 public class ModelView {
     private String view;
 
+    private Map<String, Object> attributes = new HasMap<>();
+
     public ModelView(String view) {
         this.view = view;
     }
 
+    public void addAttribute(String name, Object value) {
+        attributes.put(name, value);
+    }
+
+    public String getView() {
+        return this.view;
+    }
+    public void setView(String view){
+        this.view = view;
+    }
 }
